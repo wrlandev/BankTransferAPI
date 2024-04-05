cb81347f70c9867c23134b03b8ede8b5863e49da
wrlandev <wrlandev0@gmail.com>
2024-04-05 19:18:23 -0300

    base project

---
+++ src/main/java/com/banktransfer/BanktransferApplication.java
@@ -0,0 +1,13 @@
+package com.banktransfer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BanktransferApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BanktransferApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/banktransfer/BanktransferApplicationTests.java
@@ -0,0 +1,13 @@
+package com.banktransfer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BanktransferApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
