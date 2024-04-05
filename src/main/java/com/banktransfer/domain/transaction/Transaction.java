20683a2e8ff738e426ca556c47697b255148e002
wrlandev <wrlandev0@gmail.com>
2024-04-05 19:39:48 -0300

    feat: create Transaction Entity

---
+++ src/main/java/com/banktransfer/domain/transaction/Transaction.java
@@ -0,0 +1,31 @@
+package com.banktransfer.domain.transaction;
+
+import com.banktransfer.domain.user.User;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity(name="transactions")
+@Table(name="transactions")
+@Getter
+@Setter
+@AllArgsConstructor
+@EqualsAndHashCode(of="id")
+public class Transaction {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private BigDecimal amount;
+    @ManyToOne
+    @JoinColumn(name="sender_id")
+    private User sender;
+    @ManyToOne
+    @JoinColumn(name="reciver_id")
+    private User reciver;
+    private LocalDateTime timestamp;
+}
