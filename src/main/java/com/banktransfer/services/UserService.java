72d33dba0b7aab04aac16a294da50a44fa8ab763
wrlandev <wrlandev0@gmail.com>
2024-04-05 20:44:57 -0300

    feat: create User Service

---
+++ src/main/java/com/banktransfer/services/UserService.java
@@ -0,0 +1,33 @@
+package com.banktransfer.services;
+
+import com.banktransfer.domain.user.User;
+import com.banktransfer.enums.UserType;
+import com.banktransfer.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+
+@Service
+public class UserService {
+    @Autowired
+    private UserRepository repository;
+
+    public void validateTransaction(User sender, BigDecimal amount) throws Exception {
+        if (sender.getUserType() == UserType.MERCHANT) {
+            throw new Exception("Usuário do tipo Logista não está autorizado a realizar transação");
+        }
+
+        if (sender.getBalance().compareTo(amount) < 0) {
+            throw new Exception("Saldo insuficiente");
+        }
+    }
+
+    public User findUserById(Long id) throws Exception {
+        return this.repository.findUSerById(id).orElseThrow(() -> new Exception("Usuário não encontrado"));
+    }
+
+    public void saveUser(User user){
+        this.repository.save(user);
+    }
+}
