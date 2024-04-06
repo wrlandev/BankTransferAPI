66542ad55331c2d8aa604eccd5316dc4bbb7acdb
wrlandev <wrlandev0@gmail.com>
2024-04-05 23:09:07 -0300

    feat: create User DTO

---
+++ src/main/java/com/banktransfer/dtos/UserDTO.java
@@ -0,0 +1,8 @@
+package com.banktransfer.dtos;
+
+import com.banktransfer.enums.UserType;
+
+import java.math.BigDecimal;
+
+public record UserDTO(String firstName, String lastName, String document, BigDecimal balance, String email, String password, UserType userType) {
+}
