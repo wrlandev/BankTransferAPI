e9923bd58938bd2f982d1c8a0fe2ee76a88f6f5c
wrlandev <wrlandev0@gmail.com>
2024-04-05 23:37:53 -0300

    feat(transaction/Service): Apply Notification Service

--- src/main/java/com/banktransfer/services/TransactionService.java
+++ src/main/java/com/banktransfer/services/TransactionService.java
@@ -24,7 +24,10 @@ public class TransactionService {
     @Autowired
     private RestTemplate restTemplate;
 
-    public void createTransaction(TransactionDTO transaction) throws Exception {
+    @Autowired
+    private NotificationService notificationService;
+
+    public Transaction createTransaction(TransactionDTO transaction) throws Exception {
         User sender = this.userService.findUserById(transaction.senderId());
         User receiver = this.userService.findUserById(transaction.receiverId());
 
@@ -47,6 +50,11 @@ public class TransactionService {
         this.repository.save(newTransaction);
         this.userService.saveUser(sender);
         this.userService.saveUser(receiver);
+
+        this.notificationService.sendNotification(sender, "Transação realizada com sucesso");
+        this.notificationService.sendNotification(receiver, "Transação realizada com sucesso");
+
+        return newTransaction;
     }
 
     public boolean authorizeTransaction(User sender, BigDecimal value){
