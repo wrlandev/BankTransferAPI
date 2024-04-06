013c56f34f10d318d2bc4a7b072ef5ec34b362b4
wrlandev <wrlandev0@gmail.com>
2024-04-05 22:10:33 -0300

    feat: create Notification Service

---
+++ src/main/java/com/banktransfer/services/NotificationService.java
@@ -0,0 +1,27 @@
+package com.banktransfer.services;
+
+import com.banktransfer.domain.user.User;
+import com.banktransfer.dtos.NotificationDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+public class NotificationService {
+    @Autowired
+    private RestTemplate restTemplate;
+
+    public void sendNotification(User user, String message) throws Exception {
+        String email = user.getEmail();
+        NotificationDTO notificationRequest = new NotificationDTO(email, message);
+
+        ResponseEntity<String> notificationResponse = restTemplate.postForEntity("https://run.mocky.io/v3/54dc2cf1-3add-45b5-b5a9-6bf7e7f1f4a6", notificationRequest, String.class);
+
+        if(!(notificationResponse.getStatusCode() == HttpStatus.OK)){
+            System.out.println("erro ao enviar notificação");
+            throw new Exception("Serviço de notificação está fora do ar");
+        }
+    }
+}
