2633013d5f952bccf54a74a5bf72caf61fd07e61
wrlandev <wrlandev0@gmail.com>
2024-04-05 23:08:22 -0300

    feat(service/User): create and get all users

--- src/main/java/com/banktransfer/services/UserService.java
+++ src/main/java/com/banktransfer/services/UserService.java
@@ -1,12 +1,14 @@
 package com.banktransfer.services;
 
 import com.banktransfer.domain.user.User;
+import com.banktransfer.dtos.UserDTO;
 import com.banktransfer.enums.UserType;
 import com.banktransfer.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
+import java.util.List;
 
 @Service
 public class UserService {
@@ -25,6 +27,16 @@ public class UserService {
 
     public User findUserById(Long id) throws Exception {
         return this.repository.findUSerById(id).orElseThrow(() -> new Exception("Usuário não encontrado"));
+    }
+
+    public User createUser(UserDTO data){
+        User newUser = new User(data);
+        this.saveUser(newUser);
+        return newUser;
+    }
+
+    public List<User> getAllUsers() {
+        return this.repository.findAll();
     }
 
     public void saveUser(User user){
