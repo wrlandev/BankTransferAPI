dcc43f8283a1ec99be7ae768eb368727fa019a9b
wrlandev <wrlandev0@gmail.com>
2024-04-05 23:07:02 -0300

    feat(domain/User): apply User DTO constructor

--- src/main/java/com/banktransfer/domain/user/User.java
+++ src/main/java/com/banktransfer/domain/user/User.java
@@ -1,11 +1,9 @@
 package com.banktransfer.domain.user;
 
+import com.banktransfer.dtos.UserDTO;
 import com.banktransfer.enums.UserType;
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 import java.math.BigDecimal;
 
@@ -14,6 +12,7 @@ import java.math.BigDecimal;
 @Getter
 @Setter
 @AllArgsConstructor
+@NoArgsConstructor
 @EqualsAndHashCode(of="id")
 public class User {
     @Id
@@ -29,4 +28,14 @@ public class User {
     private BigDecimal balance;
     @Enumerated(EnumType.STRING)
     private UserType userType;
+
+    public User(UserDTO data) {
+        this.firstName = data.firstName();
+        this.lastName = data.lastName();
+        this.balance = data.balance();
+        this.userType = data.userType();
+        this.password = data.password();
+        this.document = data.document();
+        this.email = data.email();
+    }
 }
